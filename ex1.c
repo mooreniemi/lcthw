36ad16067c3318b1471922d1277a89c58e6cf19b
Alex Moore-Niemi <amooreniemi@zipcar.com>
2015-08-27 13:37:57 -0400

    pointer section of http://computer.howstuffworks.com/c23.htm/printable

--- ex1.c
+++ ex1.c
@@ -2,52 +2,17 @@
 
 int main(int argc, char *argv[])
 {
-  int numbers[4] = {0};
-  char name[4] = {'a'};
+  int inty, jinty;
+  int *pointy; /* a pointer to an integer */
 
-  //first, print them out raw
-  printf("numbers: %d %d %d %d\n",
-      numbers[0], numbers[1],
-      numbers[2], numbers[3]);
+  pointy = &inty; /* get mem address of inty and give it to pointy as its value */
+  *pointy = 5; /* set value of mem address you got from inty to 5 */
 
-  printf("name each: %c %c %c %c\n",
-      name[0], name[1],
-      name[2], name[3]);
+  printf("actual value of pointy is mem address: %d\n", pointy);
+  jinty = inty; /* copy value of inty to jinty */
 
-  printf("name: %s\n", name);
-
-  //setup the numbers
-  numbers[0] = 1;
-  numbers[1] = 2;
-  numbers[2] = 3;
-  numbers[3] = 4;
-
-  //setup the name
-  name[0] = 'Z';
-  name[1] = 'e';
-  name[2] = 'd';
-  name[3] = '\0';
-
-  //then print them out initialized
-  printf("numbers: %d %d %d %d\n",
-      numbers[0], numbers[1],
-      numbers[2], numbers[3]);
-
-  printf("name each: %c %c %c %c\n",
-      name[0], name[1],
-      name[2], name[3]);
-
-  //print the name like a string
-  printf("name: %s\n", name);
-
-  //another way to use name
-  char *another = "Zed";
-
-  printf("another: %s\n",another);
-
-  printf("another each: %c %c %c %c\n",
-      another[0], another[1],
-      another[2], another[3]);
+  /* will output 5 5 5 */
+  printf("inty is %d and jinty is %d while *pointy is the value through the mem address to inty %d\n", inty, jinty, *pointy);
 
   return 0;
 }
