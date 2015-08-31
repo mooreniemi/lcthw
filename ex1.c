09af08c9b75ae31e6006365d7294c39ff835be5d
Alex Moore-Niemi <amooreniemi@zipcar.com>
2015-08-31 14:06:05 -0400

    Using Pointers for Function Parameters

--- ex1.c
+++ ex1.c
@@ -2,22 +2,25 @@
 
 void swap(int *i, int *j)
 {
-  int t;
-  t = *i;
+  int holding_pen;
+  // put the first int in the pen
+  holding_pen = *i;
+  // put the second int into the first position
   *i = *j;
-  *j = t;
+  // take i from the holding pen and put it in second position
+  *j = holding_pen;
 }
 
 int main()
 {
   int a,b;
 
-  a=5;
-  b=10;
+  a = 5;
+  b = 10;
 
-  printf("%d %d\n", a, b);
+  printf("preswap: %d %d\n", a, b);
   swap(&a,&b);
-  printf("%d %d\n", a, b);
+  printf("postswap: %d %d\n", a, b);
 
   return 0;
 
