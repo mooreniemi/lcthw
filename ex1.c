6f573fece15087c8e094e8e4a2667a25bb53a71b
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-03-12 12:33:16 -0500

    style

--- ex1.c
+++ ex1.c
@@ -12,9 +12,14 @@ void swap(int *first_address, int *second_address) {
   *second_address = holding_pen;
 }
 
+/*
+  Swaps two integers passed in, ignores any more than
+  2 values. Errors on any less.
+ */
 int main(int argc, char *argv[]) {
   if(argc <= 2) {
     printf("you need 2 things to swap\n");
+
     return 1;
   } else {
     int a, b;
