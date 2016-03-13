bd65cea0a6394303df72763bec6a0218472ba9af
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-03-13 16:15:23 -0400

    more assertions

--- assertions.c
+++ assertions.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <assert.h>
 
+// https://en.wikipedia.org/wiki/Assert.h
 int test_assert(int x) {
-  assert(x <= 4);
+  assert(x != 4);
   return x;
 }
 
 int main(int argc, char *argv[])
 {
   int i;
+  int j;
 
-  for (i = 0; i < 9; ++i) {
-    test_assert(i);
+  // i effectively bounds both
+  for (i = 0, j = 10; i < 9; ++i, --j) {
+    printf("j = %d\n", j);
+    test_assert(j);
     printf("i = %d\n", i);
+    test_assert(i);
   }
 
   return 0;
