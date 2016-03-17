3d9168f41f1c89c1cfcc2e8282c3dbd1e70d8818
Alex Moore-Niemi <amooreniemi@zipcar.com>
2016-03-17 11:45:43 -0400

    hex, remove ds store

---
+++ hex.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hex.c
+ *
+ *    Description:  convert decimals to hex
+ *
+ *        Version:  1.0
+ *        Created:  03/16/2016 16:34:22
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author: rishabh kedia
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+int main() {
+    unsigned int i;
+    scanf("%x",&i);
+    printf("%d",i);
+}
